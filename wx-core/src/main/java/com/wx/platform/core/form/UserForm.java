b260cceb4fff44b95d4d7d106945655d4804fcc3
Yongyong.Wang <Yongyong.Wang@connext.com.cn>
2018-08-24 17:26:12 +0800

    wx 调整

---
+++ wx-core/src/main/java/com/wx/platform/core/form/UserForm.java
@@ -0,0 +1,23 @@
+package com.wx.platform.core.form;
+
+public class UserForm {
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+}
