38b8ee17fd87f1614ad2c9d208bfecc2122a69ed
Yongyong.Wang <Yongyong.Wang@connext.com.cn>
2018-08-24 15:39:32 +0800

    wx 调整

---
+++ wx-core/src/main/java/com/wx/platform/core/model/WxAccountConfigModel.java
@@ -0,0 +1,124 @@
+package com.wx.platform.core.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "wx_account_config")
+public class WxAccountConfigModel implements Serializable {
+
+    @Id
+    @Column(name = "wx_id")
+    private String wxId;
+
+    @Column(name = "wx_name")
+    private String wxName;
+
+    @Column(name = "wx_source_id")
+    private String wxSourceId;
+
+    @Column(name = "wx_number")
+    private String wxNumber;
+
+    @Column(name = "wx_token")
+    private String wxToken;
+
+    @Column(name = "wx_app_id")
+    private String wxAppId;
+
+    @Column(name = "wx_app_secret")
+    private String wxAppSecret;
+
+    @Column(name = "wx_sort")
+    private Integer wxSort;
+
+    @Column(name = "wx_is_valid")
+    private Integer wxIsValid;
+
+    @Column(name = "wx_description")
+    private String wxDescription;
+
+    public String getWxId() {
+        return wxId;
+    }
+
+    public void setWxId(String wxId) {
+        this.wxId = wxId;
+    }
+
+    public String getWxName() {
+        return wxName;
+    }
+
+    public void setWxName(String wxName) {
+        this.wxName = wxName;
+    }
+
+    public String getWxSourceId() {
+        return wxSourceId;
+    }
+
+    public void setWxSourceId(String wxSourceId) {
+        this.wxSourceId = wxSourceId;
+    }
+
+    public String getWxNumber() {
+        return wxNumber;
+    }
+
+    public void setWxNumber(String wxNumber) {
+        this.wxNumber = wxNumber;
+    }
+
+    public String getWxToken() {
+        return wxToken;
+    }
+
+    public void setWxToken(String wxToken) {
+        this.wxToken = wxToken;
+    }
+
+    public String getWxAppId() {
+        return wxAppId;
+    }
+
+    public void setWxAppId(String wxAppId) {
+        this.wxAppId = wxAppId;
+    }
+
+    public String getWxAppSecret() {
+        return wxAppSecret;
+    }
+
+    public void setWxAppSecret(String wxAppSecret) {
+        this.wxAppSecret = wxAppSecret;
+    }
+
+    public Integer getWxSort() {
+        return wxSort;
+    }
+
+    public void setWxSort(Integer wxSort) {
+        this.wxSort = wxSort;
+    }
+
+    public Integer getWxIsValid() {
+        return wxIsValid;
+    }
+
+    public void setWxIsValid(Integer wxIsValid) {
+        this.wxIsValid = wxIsValid;
+    }
+
+    public String getWxDescription() {
+        return wxDescription;
+    }
+
+    public void setWxDescription(String wxDescription) {
+        this.wxDescription = wxDescription;
+    }
+
+}
